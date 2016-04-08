eeacd98fb61c84a83404381a27e9ec4af0ad8e26
suyi <sdbzsuyi@gmail.com>
2016-04-08 14:42:59 +0800

    手机号验证规则扩展

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        //自定义表单验证规则
+        Validator::extend('phone',function($attribute, $value, $parameters){
+
+            $reg = "/^0?(13[0-9]|15[0-9]|17[0-9]|14[0-9]|18[0-9])[0-9]{8}$/";
+            $res = preg_match($reg, $value);
+            return $res;
+        });
     }
 
     /**
